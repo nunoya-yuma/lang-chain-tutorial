abd3a5782e24d0ab4aec902ae064cf3f6db4819d
nunoya-yuma <nuno.yu.3838@gmail.com>
2025-04-28 17:46:31 +0900

    Initial commit

---
+++ src/openai_sample.py
@@ -0,0 +1,8 @@
+import dotenv
+from langchain.chat_models import init_chat_model
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+    model = init_chat_model("gpt-4o-mini", model_provider="openai")
+    res = model.invoke("Hello, world!")
+    print(res)
